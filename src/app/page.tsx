edf1ca74f91ec593d3ea880c9e58bc3edd27ef8e
Max Himmel <himmelmax@gmail.com>
2024-10-03 10:32:54 -0700

    WIP
    + the custom call api doesnt appear to work as it looks. ugh.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,39 @@ export default async function Home() {
   return (
     <HydrateClient>
       <main>
-        <form
-          action={async (formData) => {
-            "use server";
+        <div className="hero">
+          <div className="hero-content flex flex-col">
+            <form
+              action={async (formData) => {
+                "use server";
 
-            console.log("Dialing voicemail...");
-            await api.voice.makeCall();
-            console.log("Voicemail dialed.");
-          }}
-        >
-          <button type="submit" className="btn btn-lg">
-            Dial Voicemail
-          </button>
-        </form>
+                console.log("Dialing voicemail...");
+                await api.voice.ttsCall({
+                  text: "Hello, Bah-tee, happy birthday. You are my best bah-tee. I love you.",
+                });
+                console.log("Voicemail dialed.");
+              }}
+            >
+              <button type="submit" className="btn btn-lg">
+                Dial Voicemail
+              </button>
+            </form>
+
+            <form
+              action={async (formData) => {
+                "use server";
+
+                console.log("Testing custom call...");
+                await api.voice.testVoice();
+                console.log("Custom call complete.");
+              }}
+            >
+              <button type="submit" className="btn btn-lg">
+                Custom Dial
+              </button>
+            </form>
+          </div>
+        </div>
       </main>
     </HydrateClient>
   );
--- src/server/api/routers/voiceRouter.ts
+++ src/server/api/routers/voiceRouter.ts
@@ -1,10 +1,14 @@
 import { env } from "@/env";
+import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "@/server/api/trpc";
-import { SinchClient } from "@sinch/sdk-core";
+import { SinchClient, Voice } from "@sinch/sdk-core";
 
 export const voiceRouter = createTRPCRouter({
-    makeCall: protectedProcedure
-        .query(async ({ ctx }) => {
+    ttsCall: protectedProcedure
+        .input(z.object({
+            text: z.string()
+        }))
+        .query(async ({ ctx, input }) => {
             const sinchClient = new SinchClient({
                 applicationKey: env.SINCH_APP_KEY,
                 applicationSecret: env.SINCH_APP_SECRET,
@@ -22,11 +26,65 @@ export const voiceRouter = createTRPCRouter({
                             type: "number",
                             endpoint: vmNumber,
                         },
-                        text: "This is a test call from Sinch using the Node.js SDK.",
+                        text: input.text,
                     },
                 },
             });
 
             console.log(JSON.stringify(response));
+
+            return { callId: response.callId };
         }),
+
+    testVoice: protectedProcedure
+        .query(async ({ ctx }) => {
+            const sinchClient = new SinchClient({
+                applicationKey: env.SINCH_APP_KEY,
+                applicationSecret: env.SINCH_APP_SECRET,
+            });
+
+            const response = await sinchClient.voice.callouts.custom({
+                customCalloutRequestBody: {
+                    method: "customCallout",
+                    customCallout: {
+                        cli: sinchNumber,
+                        // dtmf: "",
+                        destination: {
+                            type: "number",
+                            endpoint: vmNumber,
+                        },
+                        ace: JSON.stringify(new Voice.AceSvamletBuilder()
+                            .addInstruction(Voice.aceInstructionHelper.say("Howdy there, bud."))
+                            .setAction(Voice.aceActionHelper.continue())
+                            .build()),
+                    }
+                }
+            });
+
+            await sinchClient.voice.calls.update({
+                callId: response.callId as string,
+                updateCallRequestBody: {
+                    instructions: [
+                        Voice.svamlInstructionHelper.buildSay("well hello again.")
+                    ],
+                    action: Voice.svamlActionHelper.buildContinue()
+                }
+            });
+
+            await sinchClient.voice.calls.update({
+                callId: response.callId as string,
+                updateCallRequestBody: {
+                    instructions: [
+                        Voice.svamlInstructionHelper.buildSay("and now we're done. goodbye.")
+                    ],
+                    action: Voice.svamlActionHelper.buildHangup()
+                }
+            });
+
+            // sinchClient.conversation.webhooks.create({
+            //     webhookCreateRequestBody: {
+            //         client_credentials: {}
+            //     }
+            // });
+        })
 });
