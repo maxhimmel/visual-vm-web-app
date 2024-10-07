b63757a343448fbb7a873bf249ef388cc234c66e
Max Himmel <himmelmax@gmail.com>
2024-10-07 10:22:59 -0700

    create 1st pass of dialing/recording VM message
    + unfortunately, this doesn't loop when there are multiple messages ...

--- src/server/api/routers/voiceRouter.ts
+++ src/server/api/routers/voiceRouter.ts
@@ -82,4 +82,52 @@ export const voiceRouter = createTRPCRouter({
 
             return recordingData;
         }),
+
+    dialVoicemail: protectedProcedure
+        .mutation(async ({ ctx }) => {
+            const client = new Twilio(env.TWILIO_ACCOUNT_SID, env.TWILIO_AUTH_TOKEN);
+
+            // TODO: How do we loop this twiml until the voicemail is empty?
+            const twiml = new TWIML.VoiceResponse();
+
+            twiml.play({
+                digits: `w${ctx.voicemail?.userNumber}#w${ctx.voicemail?.vmPin}#`
+            });
+
+            twiml.gather({
+                input: ["speech"],
+                speechTimeout: "auto",
+                hints: "New message"
+            });
+
+            twiml.record({
+                recordingStatusCallback: recordingCallback,
+                maxLength: 120, // Twilio's max transcription length.
+                playBeep: false,
+                transcribe: true,
+                trim: "trim-silence",
+            });
+
+            twiml.play({
+                digits: `${deleteVm}w`
+            });
+
+            twiml.hangup();
+
+            const call = await client.calls.create({
+                from: env.TWILIO_PHONE_NUMBER,
+                to: `+1${ctx.voicemail?.vmNumber}`,
+                twiml,
+
+                //// These are interesting ...
+                // machineDetectionSpeechEndThreshold,
+                // machineDetection,
+                // machineDetectionSpeechThreshold
+            });
+
+            return call;
+        })
 });
+
+const saveVm = "1";
+const deleteVm = "7";
