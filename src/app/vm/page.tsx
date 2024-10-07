2351d844f1ebdd21b5a5130f2f82a0e1be8d8c31
Max Himmel <himmelmax@gmail.com>
2024-10-07 10:20:31 -0700

    create vm testing/viewing page

---
+++ src/app/vm/page.tsx
@@ -0,0 +1,46 @@
+import { api, HydrateClient } from "@/trpc/server";
+
+export default async function Voicemails() {
+  const recordings = await api.voice.getRecordings();
+
+  return (
+    <HydrateClient>
+      <main>
+        <div className="hero">
+          <div className="hero-content flex flex-col">
+            <form
+              action={async (formData) => {
+                "use server";
+
+                console.log("Dialing voicemail...");
+                await api.voice.testCall();
+                console.log("Voicemail dialed.");
+              }}
+            >
+              <button type="submit" className="btn btn-lg">
+                Dial Voicemail
+              </button>
+            </form>
+
+            <div>
+              <h2 className="text-4xl">Recordings</h2>
+              <ul className="space-y-4">
+                {recordings.map((recording) => (
+                  <li key={recording.recording.sid}>
+                    <audio controls>
+                      <source
+                        src={recording.recording.mediaUrl}
+                        type="audio/mp3"
+                      />
+                    </audio>
+                    <code>{recording.transcript}</code>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
