b0f1d1200e3a9af5198a4648d0b54032fac9bc6e
Max Himmel <himmelmax@gmail.com>
2024-10-12 15:36:23 -0700

    cleanup

--- src/lib/twimlHelpers.ts
+++ src/lib/twimlHelpers.ts
@@ -1,46 +1,5 @@
-import { env } from "@/env";
 import { twiml as TWIML } from "twilio";
-
-const recordingCallback = `${env.API_URL}/webhooks/record`;
-const transcriptionCallback = `${env.API_URL}/webhooks/transcribe`;
-
 export class TwimlHelpers {
-    static record({
-        twiml,
-        gatherHints,
-        gatherDelay = 2,
-        recordingAction,
-    }: {
-        twiml: TWIML.VoiceResponse;
-        gatherHints: string;
-        gatherDelay: number;
-        recordingAction: string;
-    }) {
-
-        twiml.gather({
-            input: ["speech"],
-            speechTimeout: "auto",
-            profanityFilter: false,
-            hints: gatherHints,
-        });
-
-        twiml.pause({
-            length: gatherDelay
-        });
-
-        twiml.record({
-            recordingStatusCallback: recordingCallback,
-            transcribeCallback: transcriptionCallback,
-            maxLength: 120, // Twilio's max transcription length.
-            playBeep: false,
-            transcribe: true,
-            trim: "trim-silence",
-            action: recordingAction,
-        });
-
-        return twiml;
-    }
-
     static deleteVm({ twiml }: { twiml: TWIML.VoiceResponse }) {
         twiml.play({
             digits: `7`,
