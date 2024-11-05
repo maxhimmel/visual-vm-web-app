ca85094f10d93adcc2f68bc9fe529ad389348a22
Max Himmel <himmelmax@gmail.com>
2024-11-05 10:40:42 -0800

    add google android auth
    + this probably isn't gonna work

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -52,6 +52,10 @@ export const authOptions: NextAuthOptions = {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
+    Google({
+      clientId: env.GOOGLE_CLIENT_ANDROID_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
     /**
      * ...add more providers here.
      *
